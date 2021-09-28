a54fe0ca93a1fa9dad622ab76a83aac7e42a4b44
FabriceSerapion <fabrice-serapion_student2021@wilder.school>
2021-09-28 11:46:38 +0200

    create repo Vache qui parle

---
+++ index.js
@@ -0,0 +1,9 @@
+const cowsay = require("cowsay");
+
+const userInfo = require("./information");
+
+console.log(cowsay.say({
+    text : `Hello I'm ${userInfo.name} from ${userInfo.campus} campus`,
+    e : "oO",
+    T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,9 @@
+const userInfo = {
+    name: "Fabrice Sérapion",
+    campus: "Strasbourg",
+}
+
+module.exports = {
+    name: userInfo.name,
+    campus: userInfo.campus,
+};
